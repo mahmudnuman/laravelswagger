23566b3934ee1ec25935bdc6e7c380abc6388eaa
mahmudnuman <mahmudnuman@gmail.com>
2023-08-17 00:09:53 +0600

    swagger

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -1,9 +1,13 @@
 <?php 
 
 
+namespace Tests\Feature;
+
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
+use App\Models\Article;
 use App\Models\User;
 
 class ArticleTest extends TestCase
@@ -11,12 +15,29 @@ class ArticleTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function logged_in_user_can_create_article()
+    public function user_can_retrieve_articles()
     {
         $user = User::factory()->create();
+
+        Article::factory(3)->create();
+
+        $response = $this->actingAs($user)
+            ->getJson('/api/articles');
+
+        $response->assertStatus(200)
+            ->assertJsonCount(3); // Assuming you created 3 articles
+    }
+
+    /** @test */
+    public function user_can_create_article()
+    {
+        $user = User::factory()->create([
+            'password' => Hash::make('password'),
+        ]);
+
         $articleData = [
-            'title' => 'New Article Title',
-            'description' => 'New Article Description',
+            'title' => 'Test Article',
+            'description' => 'This is a test article.',
         ];
 
         $response = $this->actingAs($user)
@@ -24,7 +45,43 @@ class ArticleTest extends TestCase
 
         $response->assertStatus(201)
             ->assertJsonFragment($articleData);
+    }
 
-        $this->assertDatabaseHas('articles', $articleData);
+    /** @test */
+    public function user_can_update_article()
+    {
+        $user = User::factory()->create([
+            'password' => Hash::make('password'),
+        ]);
+
+        $article = Article::factory()->create();
+
+        $updatedData = [
+            'title' => 'Updated Title',
+            'description' => 'Updated description.',
+        ];
+
+        $response = $this->actingAs($user)
+            ->putJson("/api/articles/{$article->id}", $updatedData);
+
+        $response->assertStatus(200)
+            ->assertJsonFragment($updatedData);
+    }
+
+    /** @test */
+    public function user_can_delete_article()
+    {
+        $user = User::factory()->create([
+            'password' => Hash::make('password'),
+        ]);
+
+        $article = Article::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->deleteJson("/api/articles/{$article->id}");
+
+        $response->assertStatus(204);
+
+        $this->assertDatabaseMissing('articles', ['id' => $article->id]);
     }
 }
